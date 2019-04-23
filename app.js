612422bdc822cc5d1e7ebb644a2f368655e81448
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-04-23 11:32:44 -0600

    Initialize express server and port address

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // It only needs to accept a POST request to the route “/test” which accepts one argument “string_to_cut” returns a JSON object with the key “return_string” and a string containing every third letter from the original string. E.g. if you POST {"string_to_cut": "iamyourlyftdriver"}, it will return: {"return_string": "muydv"}.] To see expected behavior you can test against a current working example with the command: curl -X POST https://lyft-interview-test.herokuapp.com/test --data '{"string_to_cut": "iamyourlyftdriver"}' -H 'Content-Type: application/json'
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Test Server';
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Listen on PORT
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`);
+});
