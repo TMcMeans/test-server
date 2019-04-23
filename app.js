a8500c35cafe9649b553ac663b0c7982e625683e
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-04-23 12:48:01 -0600

    Add help method for cutting string

--- app.js
+++ app.js
@@ -13,13 +13,17 @@ app.locals.title = 'Test Server';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//A method for returning a new string with every 3rd char from the argument string
+//A method for returning a new string with every 3rd char from the argument string included
 const cutThisString = str => {
   let cutString = '';
   let chars = str.split('');
-  for (let i = 0; i < chars.length; i++) {}
+  for (let i = 0; i < chars.length; i++) {
+    if (i % 3 === 0) {
+      cutString += chars[i];
+    }
+  }
 
-  return cutString;
+  return { return_string: cutString };
 };
 
 //POST handler accepts a string as the request body, calls the cutThisString method upon successful acceptance and sends a modified string as a response
@@ -27,7 +31,7 @@ app.post('/test', (req, res) => {
   const { string_to_cut } = req.body;
   if (string_to_cut) {
     //Send modified string back as a response
-    res.send(cutThisString(string_to_cut));
+    res.json(cutThisString(string_to_cut));
   } else {
     res.status(400).send({
       error: 'You are missing data! Expected format: {string_to_cut: <string> }'
